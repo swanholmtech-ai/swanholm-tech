bed9922a4b0691dbebbde54895840378e27fa386
Swanholm Technology <swanholmtech@gmail.com>
2025-10-06 16:38:38 +0200

    fix some navigation bar css

--- components/view/navbar/NavigationBar.tsx
+++ components/view/navbar/NavigationBar.tsx
@@ -3,25 +3,25 @@ import Link from 'next/link';
 
 export function NavigationBar() {
   return (
-    <nav>
-      <ul className="flex gap-4">
-        <li>
+    <nav className="bg-slate-600 mx-auto max-w-xl text-gray-200 rounded-xs h-8">
+      <ul className="flex gap-4 items-center justify-evenly pt-1">
+        <li className=" hover:text-gray-400">
           <Link href="/">Home</Link>
         </li>
 
-        <li>
+        <li className=" hover:text-gray-400">
           <Link href="/products">Products</Link>
         </li>
 
-        <li>
+        <li className=" hover:text-gray-400">
           <Link href="/about">About</Link>
         </li>
 
-        <li>
+        <li className=" hover:text-gray-400">
           <Link href="/news">News</Link>
         </li>
         
-        <li>
+        <li className=" hover:text-gray-400">
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
