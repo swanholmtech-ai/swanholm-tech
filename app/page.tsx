37c4113c1eb79dedda12d72b488964ab0365aa28
Swanholm Technology <swanholmtech@gmail.com>
2025-10-07 11:21:25 +0200

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import HeroSection from "@/components/view/home/HeroSection";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="max-w-7xl mx-auto">
         <HeroSection />
+        <Footer />
       </main>
-      <Footer />
-    </div>
   );
 }
