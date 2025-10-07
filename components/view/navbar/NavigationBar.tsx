b19f9a7a3d8a0da6aea660c416b7a5472ef45988
Swanholm Technology <swanholmtech@gmail.com>
2025-10-07 13:02:10 +0200

    save

--- components/view/navbar/NavigationBar.tsx
+++ components/view/navbar/NavigationBar.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 
 export function NavigationBar() {
   return (
-    <nav className="bg-slate-600 mx-auto max-w-xl text-gray-200 rounded-xs h-8 z-50 my-2 fixed top-0 left-0 right-0">
+    <nav className="bg-zinc-200 mx-auto max-w-xl text-gray-500 rounded-xs h-9 z-50 my-2 fixed top-0 left-0 right-0 border-1 border-gray-100"
+      style={{
+        boxShadow: "-1px 1px 3px 1px rgba(0, 0, 0, 0.5)",
+      }}>
       <ul className="flex gap-4 items-center justify-evenly pt-1">
         <li className=" hover:text-gray-400">
           <Link href="/">Home</Link>
