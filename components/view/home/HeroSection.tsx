98d68199a8fb7b938843c7c22bbeb7aa0d639cdb
Swanholm Technology <swanholmtech@gmail.com>
2025-10-07 12:49:36 +0200

    save

--- components/view/home/HeroSection.tsx
+++ components/view/home/HeroSection.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="w-full h-[100dvh] flex border-2 border-gray-300 rounded-md">
-      <div className="flex-3 bg-slate-100">
+    <section className="w-full h-[100dvh] flex border-2 border-gray-300 rounded-md bg-slate-100">
+      <div className="flex-3">
         <div className="px-12 pt-40 h-full">
           <h4 className="text-5xl font-bold h-fit overflow-y-hidden">What if your Vest could think?</h4>
           <p className="text-lg pt-8">
@@ -11,14 +11,16 @@ export default function HeroSection() {
           </p>
         </div>
       </div>
-      <div className="flex-2 bg-cyan-950 flex items-center justify-center">
+      <div className="flex-2 flex items-center justify-center">
+        <div className="bg-amber-700 w-120 h-120 rounded-full flex items-center justify-center">
         <Image
           src="assets/images/svgs/vest.svg"
           alt="Vest"
           width={300}
           height={300}
           priority={true}
-        />
+          />
+          </div>
       </div>
     </section>
   );
