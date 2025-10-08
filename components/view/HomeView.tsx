5c806051db43142befe59930453cd1f0afde0f69
Swanholm Technology <swanholmtech@gmail.com>
2025-10-08 12:03:02 +0200

    add second section

---
+++ components/view/HomeView.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./home-sections/HeroSection";
+import ProblemsSection from "./home-sections/ProblemsSection";
+
+export default function HomeView() {
+  return (
+    <>
+      <HeroSection />
+      <ProblemsSection />
+    </>
+  );
+}
