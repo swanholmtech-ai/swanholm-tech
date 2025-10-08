3a5687be25ad66feb53c24e6bdeb7b1348788d60
Swanholm Technology <swanholmtech@gmail.com>
2025-10-08 10:28:24 +0200

    Implemented SolutionSection

--- components/view/home/HomePage.tsx
+++ components/view/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./HeroSection";
 import ProblemsSection from "./ProblemsSection";
+import SolutionSection from "./SolutionSection";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection/>
       <ProblemsSection />
+      <SolutionSection />
     </>
   );
 }
---
+++ components/view/home/SolutionSection.tsx
@@ -0,0 +1,34 @@
+
+export default function SolutionSection() {
+  return (
+    <section
+      className="w-full h-[100dvh] flex mt-14 border-2 border-gray-300 rounded-md relative overflow-hidden"
+    >
+      <div
+        className="absolute inset-0 z-0"
+      />
+      <div className="relative z-10 w-full flex">
+        <div className="flex-1">
+          <div className="px-12 pt-40 h-full">
+            <h4 className="text-5xl font-bold h-fit overflow-y-hidden text-amber-700">Work Accident Happen Every day</h4>
+            <p className="text-lg pt-8">
+            Work Confidently. Your Smart Vest Has Your Back.
+            </p>
+            <p className="text-lg pt-8">
+            AI Fall Detection
+            </p>
+            <p className="text-lg pt-8">
+            Three Alarm Type
+            </p>
+            <p className="text-lg pt-8">
+            Swanholm Sense App
+            </p>
+            <p className="text-lg pt-8">
+            Privacy First            </p>
+          </div>
+        </div>
+      <div className="flex-1"></div>
+      </div>
+    </section>
+  );
+}
