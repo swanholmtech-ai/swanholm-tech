cd772bb58f821520a5c3537693cea5ce9af91e62
Swanholm Technology <swanholmtech@gmail.com>
2025-10-08 17:25:55 +0200

    Add font

--- components/view/home-sections/SolutionSection.tsx
+++ components/view/home-sections/SolutionSection.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+const poppinsBold = Poppins({
+  weight: ["700"],
+  subsets: ["latin"],
+  style: ["italic"],
+});
 
 export default function SolutionSection() {
   return (
     <section className="w-full h-[100dvh] flex flex-col pt-28 border-2 border-gray-300 relative overflow-hidden">
       <div className="flex-1 mx-auto">
-        <h4 className="text-4xl font-bold h-fit overflow-y-hidden text-sky-900 text-center italic">
+        <h4
+          className={`${poppinsBold.className} text-4xl tracking-tight text-center`}
+        >
           Work Confidently,
           <br />
           Your Smart Vest Has Your Back.
@@ -13,8 +22,12 @@ export default function SolutionSection() {
       <div className="flex-4 p-12 flex items-center justify-center">
         <div className="flex justify-evenly w-full">
           <div className="flex-2 text-center flex flex-col items-center justify-center">
-            <p className="text-3xl font-bold pb-4">AI Fall Detection</p>
-            <p className="text-3xl font-bold">Three Alarm Type</p>
+            <p className={`text-3xl ${poppinsBold.className}`}>
+              AI Fall Detection
+            </p>
+            <p className={`text-3xl ${poppinsBold.className}`}>
+              Three Alarm Type
+            </p>
           </div>
           <div
             className="flex-3"
@@ -32,8 +45,12 @@ export default function SolutionSection() {
             />
           </div>
           <div className="flex-2 text-center flex flex-col items-center justify-center">
-            <p className="text-3xl font-bold pb-4">Swanholm Sense App</p>
-            <p className="text-3xl font-bold">Privacy First </p>
+            <p className={`text-3xl ${poppinsBold.className}`}>
+              Swanholm Sense App
+            </p>
+            <p className={`text-3xl ${poppinsBold.className}`}>
+              Privacy First{" "}
+            </p>
           </div>
         </div>
       </div>
