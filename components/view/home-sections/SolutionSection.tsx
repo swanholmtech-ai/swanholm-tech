2734dfe55672a45920691909fa33a0841197f737
Swanholm Technology <swanholmtech@gmail.com>
2025-10-08 13:36:45 +0200

    implemented Solution section

--- components/view/home-sections/SolutionSection.tsx
+++ components/view/home-sections/SolutionSection.tsx
@@ -4,19 +4,26 @@ export default function SolutionSection() {
   return (
     <section className="w-full h-[100dvh] flex flex-col pt-28 border-2 border-gray-300 relative overflow-hidden">
       <div className="flex-1 mx-auto">
-        <h4 className="text-4xl font-bold h-fit overflow-y-hidden text-sky-900 text-center">
+        <h4 className="text-4xl font-bold h-fit overflow-y-hidden text-sky-900 text-center italic">
           Work Confidently,
           <br />
           Your Smart Vest Has Your Back.
         </h4>
       </div>
       <div className="flex-4 p-12 flex items-center justify-center">
-        <div className="flex gap-12 ">
-          <div>
-            <p className="text-lg pt-8 font-bold">AI Fall Detection</p>
-            <p className="text-lg pt-8">Three Alarm Type</p>
+        <div className="flex justify-evenly w-full">
+          <div className="flex-2 text-center flex flex-col items-center justify-center">
+            <p className="text-3xl font-bold pb-4">AI Fall Detection</p>
+            <p className="text-3xl font-bold">Three Alarm Type</p>
           </div>
-          <div>
+          <div
+            className="flex-3"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Image
               src="/assets/svgs/vest.svg"
               alt="Solution"
@@ -24,9 +31,9 @@ export default function SolutionSection() {
               height={400}
             />
           </div>
-          <div>
-            <p className="text-lg pt-8 font-bold">Swanholm Sense App</p>
-            <p className="text-lg pt-8">Privacy First </p>
+          <div className="flex-2 text-center flex flex-col items-center justify-center">
+            <p className="text-3xl font-bold pb-4">Swanholm Sense App</p>
+            <p className="text-3xl font-bold">Privacy First </p>
           </div>
         </div>
       </div>
