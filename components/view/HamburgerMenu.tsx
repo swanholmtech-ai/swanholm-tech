d1bb648531855064127707e547c6f5f5998800ea
Swanholm Technology <swanholmtech@gmail.com>
2025-10-09 18:34:53 +0200

    Impolemented mobile hamburger manu

---
+++ components/view/HamburgerMenu.tsx
@@ -0,0 +1,84 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Menu } from "lucide-react";
+
+export function HamburgerMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        className="cursor-pointer fixed top-4 left-6 bg-orange-500 hover:bg-orange-600 rounded-sm text-white hover:text-gray-200 shadow-sm z-50"
+        style={{
+          boxShadow: "-1px 1px 2px 1px rgba(0, 0, 0, 0.7)",
+        }}
+      >
+        <Button>
+          <Menu />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Keyboard shortcuts
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>Email</DropdownMenuItem>
+                <DropdownMenuItem>Message</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>More...</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            New Team
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>GitHub</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuItem disabled>API</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- components/view/NavigationBar.tsx
+++ components/view/NavigationBar.tsx
@@ -3,71 +3,80 @@ import Link from "next/link";
 import "@/style/nav-bar.css";
 import { Home, Shirt, Store, Newspaper, Send } from "lucide-react";
 import { Button } from "../ui/button";
+import { useMediaQuery } from "@/hook/useMediaQuery";
+import { HamburgerMenu } from "./HamburgerMenu";
 
 export function NavigationBar() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
-    <nav className="mx-auto max-w-md z-50 my-2 fixed top-0 left-0 right-0">
-      <ul className="flex gap-4 items-center justify-evenly">
-        <Link href="/">
-          <Button
-            variant="default"
-            size="icon-lg"
-            className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
-          >
-            <li>
-              <Home />
-            </li>
-          </Button>
-        </Link>
+    <>
+      {isMobile ? (
+        <HamburgerMenu />
+      ) : (
+        <nav className="mx-auto max-w-md z-50 my-2 fixed top-0 left-0 right-0">
+          <ul className="flex gap-4 items-center justify-evenly">
+            <Link href="/">
+              <Button
+                variant="default"
+                size="icon-lg"
+                className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
+              >
+                <li>
+                  <Home />
+                </li>
+              </Button>
+            </Link>
 
-        <Link href="/products">
-          <Button
-            variant="default"
-            size="icon-lg"
-            className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
-          >
-            <li>
-              <Shirt />
-            </li>
-          </Button>
-        </Link>
+            <Link href="/products">
+              <Button
+                variant="default"
+                size="icon-lg"
+                className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
+              >
+                <li>
+                  <Shirt />
+                </li>
+              </Button>
+            </Link>
 
-        <Link href="/about">
-          <Button
-            variant="default"
-            size="icon-lg"
-            className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
-          >
-            <li>
-              <Store />
-            </li>
-          </Button>
-        </Link>
+            <Link href="/about">
+              <Button
+                variant="default"
+                size="icon-lg"
+                className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
+              >
+                <li>
+                  <Store />
+                </li>
+              </Button>
+            </Link>
 
-        <Link href="/news">
-          <Button
-            variant="default"
-            size="icon-lg"
-            className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
-          >
-            <li>
-              <Newspaper />
-            </li>
-          </Button>
-        </Link>
+            <Link href="/news">
+              <Button
+                variant="default"
+                size="icon-lg"
+                className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
+              >
+                <li>
+                  <Newspaper />
+                </li>
+              </Button>
+            </Link>
 
-        <Link href="/contact">
-          <Button
-            variant="default"
-            size="icon-lg"
-            className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
-          >
-            <li>
-              <Send />
-            </li>
-          </Button>
-        </Link>
-      </ul>
-    </nav>
+            <Link href="/contact">
+              <Button
+                variant="default"
+                size="icon-lg"
+                className="cursor-pointer bg-orange-500 hover:bg-orange-600 rounded-full navbar-button-shadow"
+              >
+                <li>
+                  <Send />
+                </li>
+              </Button>
+            </Link>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
