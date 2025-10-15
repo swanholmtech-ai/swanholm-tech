c1ba99ddfbb10c78e25c8afb58770c0e224e1316
Swanholm Technology <swanholmtech@gmail.com>
2025-10-15 13:39:32 +0200

    save

--- components/view/connecting-tutorial/StepOne.tsx
+++ components/view/connecting-tutorial/StepOne.tsx
@@ -1,28 +1,55 @@
-
-import Image from "next/image"
+import Image from "next/image";
 import {
-    DialogHeader,
-    DialogTitle,
-    DialogDescription,
-  } from "@/components/ui/dialog"
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
 
 const StepOne = () => {
-    return (
-        <>
-           <DialogHeader>
-                <DialogTitle>Connecting Toturial</DialogTitle>
-                <DialogDescription>
-                1. Download Swanholme sense App From Google play or App store and Install It
-                </DialogDescription>
-            </DialogHeader>
-            <div className="flex gap-2" style={{backgroundImage: `url('/assets/svgs/mobil.svg')`, backgroundSize: '200px', backgroundRepeat: 'no-repeat', opacity: 0.6,
-                backgroundPosition: 'right center', }}>
-                <Image src="/assets/svgs/google-play.svg" width={30} height={30} alt="google-play" />
-                <Image src="/assets/svgs/app-store.svg" width={30} height={30} alt="app-store" />
-            </div>
+  return (
+    <div className="flex flex-col h-full">
+      <div className="h-1/2">
+        <DialogHeader>
+          <DialogTitle>Connecting Toturial</DialogTitle>
+          <DialogDescription>
+            1. Download Swanholme sense App From Google play or App store and
+            Install It
+            <br />
+            &nbsp;&nbsp;&nbsp;Also can find the link at the website
+            <br />
+            <br />
+            2. Install and Open The App
+            <br />
+            &nbsp;&nbsp;&nbsp;Note: The Bluetooth must be on
 
-        </>
-    )
-}
+          </DialogDescription>
+        </DialogHeader>
+      </div>
+        <div
+          className="flex gap-2 h-[220px]"
+          style={{
+            backgroundImage: `url('/assets/svgs/mobil.svg')`,
+            backgroundSize: "200px",
+            backgroundRepeat: "no-repeat",
+            opacity: 0.6,
+            backgroundPosition: "right center",
+          }}
+        >
+          <Image
+            src="/assets/svgs/google-play.svg"
+            width={50}
+            height={50}
+            alt="google-play"
+          />
+          <Image
+            src="/assets/svgs/app-store.svg"
+            width={50}
+            height={50}
+            alt="app-store"
+          />
+        </div>
+    </div>
+  );
+};
 
-export default StepOne
+export default StepOne;
--- components/view/connecting-tutorial/TutorialHome.tsx
+++ components/view/connecting-tutorial/TutorialHome.tsx
@@ -10,9 +10,11 @@ import StepOne from "./StepOne"
 export function TutorialHome({open, setOpen}: {open: boolean, setOpen: (open: boolean) => void}) {
   return (
     <Dialog open={open}>
-        <DialogContent className="sm:max-w-[725px] close min-h-[400px]" >
-          <StepOne />
-          <DialogFooter>
+        <DialogContent className="sm:max-w-[725px] close min-h-[400px] flex flex-col gap-2">
+          <div>
+            <StepOne />
+          </div>
+          <DialogFooter className="h-1/6">
             <DialogClose asChild>
               <Button variant="outline" onClick={() => setOpen(false)}>Back</Button>
             </DialogClose>
