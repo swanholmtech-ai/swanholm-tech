23dbd66cd10e8e78ce3cf5587705da0d22e4d10a
Swanholm Technology <swanholmtech@gmail.com>
2025-10-21 20:36:59 +0200

    save

--- components/view/NavigationBar.tsx
+++ components/view/NavigationBar.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 
 import Link from "next/link";
 import "@/style/nav-bar.css";
+import "@/style/nav-buttons.css";
 import { Button } from "../ui/button";
 import { useMediaQuery } from "@/hook/useMediaQuery";
 import { HamburgerMenu } from "./HamburgerMenu";
@@ -21,17 +22,20 @@ export function NavigationBar() {
       ) : (
         <nav className="mx-auto max-w-md z-50 my-2 fixed top-2 left-0 right-0 bg-">
           <ul className="flex gap-4 items-center justify-evenly bg-">
-            <Link href="/">
+            {/* <div className="holographic-container">
+              <div className="holographic-card">
+                <h2>HOLOGRAM</h2>
+              </div>
+            </div>{" "} */}
+            <Link href="/" className="holographic-container">
               <Button
                 size="sm"
                 variant="default"
-                className={`cursor-pointer hover:bg-slate-500 hover:scale-108 transition-all duration-300 ${poppinsThin.className}`}
-                style={{ backgroundColor: "rgba(255, 255, 255, 0.2)" }}
+                className={`holographic-card ${poppinsThin.className} cursor-pointer`}
               >
                 <li>Home</li>
               </Button>
             </Link>
-
             <Link href="/products">
               <Button
                 size="sm"
@@ -42,7 +46,6 @@ export function NavigationBar() {
                 <li>Products</li>
               </Button>
             </Link>
-
             <Link href="/about">
               <Button
                 size="sm"
@@ -53,7 +56,6 @@ export function NavigationBar() {
                 <li>About</li>
               </Button>
             </Link>
-
             <Link href="/news">
               <Button
                 size="sm"
@@ -64,7 +66,6 @@ export function NavigationBar() {
                 <li>News</li>
               </Button>
             </Link>
-
             <Link href="/contact">
               <Button
                 size="sm"
