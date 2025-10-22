d8e1a2f5fcd4556a4470969f31e31f60d825657b
Swanholm Technology <swanholmtech@gmail.com>
2025-10-22 14:35:40 +0200

    save

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,8 +1,16 @@
 const ProductPage = async ({ params }: { params: { slug: string } }) => {
   const { slug } = await params;
+
+  const data = await fetch(
+    `https://www.swanholmtech.com/wp-json/wp/v2/product/${slug}`
+  );
+  const productData = await data.json();
+
   return (
     <main className="max-w-7xl mt-20 mx-auto h-[calc(100vh-5rem)]">
-      <h1 className="text-3xl font-bold mb-6">Product Page {slug}</h1>
+      <h1 className="text-3xl font-bold mb-6">
+        Product Page {productData.title.rendered}
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
     </main>
   );
