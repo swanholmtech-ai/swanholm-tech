360b6d968a82fe7402414838c5a8cc69da17e0d3
Swanholm Technology <swanholmtech@gmail.com>
2025-10-22 14:33:16 +0200

    save

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,7 +1,9 @@
-const ProductPage = ({ params }: { params: { slug: string } }) => {
+const ProductPage = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = await params;
   return (
     <main className="max-w-7xl mt-20 mx-auto h-[calc(100vh-5rem)]">
-      <h1>Product Page {params.slug}</h1>
+      <h1 className="text-3xl font-bold mb-6">Product Page {slug}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
     </main>
   );
 };
