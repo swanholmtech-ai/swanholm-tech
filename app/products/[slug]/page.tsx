6591352e7259f94f21bcaee96b229be55662af71
Swanholm Technology <swanholmtech@gmail.com>
2025-10-23 14:02:20 +0200

    save

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,3 +1,15 @@
+// import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+// const poppinsRegular = Poppins({
+//   weight: ["400"],
+//   subsets: ["latin"],
+// });
+
+const poppinsThin = Poppins({
+  weight: ["200"],
+  subsets: ["latin"],
+});
 const ProductPage = async ({ params }: { params: { slug: string } }) => {
   const { slug } = await params;
 
@@ -11,10 +23,16 @@ const ProductPage = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <main className="max-w-7xl mt-20 mx-auto h-[calc(100vh-5rem)]">
-      <h1 className="text-3xl font-bold mb-6">
-        Product Page {productData.title.rendered}
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        {productData.title.rendered}
+        <p className={`text-m text-slate-200 ${poppinsThin.className}`}>
+          {productData.content.rendered}
+        </p>
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-slate-100 w-full">
+        <div className="col-span-2 bg-slate-200">Hellos</div>
+        <div className="col-span-1 bg-slate-200"></div>
+      </div>
     </main>
   );
 };
