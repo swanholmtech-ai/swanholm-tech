89c893c2ffd6ce53da5ede51a0e0af725e4be5a1
Swanholm Technology <swanholmtech@gmail.com>
2025-11-05 23:49:47 +0100

    save

--- components/view/home-sections/problem-section/StepCard.tsx
+++ components/view/home-sections/problem-section/StepCard.tsx
@@ -1,61 +1,59 @@
 import { Nunito_Sans } from "next/font/google";
 
 const nunitoRegular = Nunito_Sans({
-  weight: ["600"],
+  weight: ["400"],
   subsets: ["latin"],
-  style: ["normal"],
 });
 
 const nunitoBold = Nunito_Sans({
-  weight: ["700"],
+  weight: ["400"],
   subsets: ["latin"],
-  style: ["italic"],
 });
 
 const StepCard = () => {
   return (
     <article className="w-full h-full">
-      <div className="w-full flex gap-4 justify-center items-center">
-        <div className="w-[50%] bg-emerald-100 p-4 m-4 rounded-lg">
+      <div className="w-full flex gap-4 justify-center items-center py-4">
+        <div className="w-[50%] bg-emerald-100 p-4 m-4 rounded-lg h-full">
           <h2
-            className={`${nunitoBold.className} text-xl md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
+            className={`${nunitoBold.className} text-lg md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
           >
             €550 B = 3.3% of EU GDP
           </h2>
-          <p>
+          <p className={`${nunitoRegular.className} text-lg text-gray-800`}>
             lost each year to workplace injuries and diseases. (Source: EU-OSHA)
           </p>
         </div>
-        <div className="w-[50%] bg-fuchsia-200 p-4 m-4 rounded-lg">
+        <div className="w-[50%] bg-fuchsia-200 p-4 m-4 rounded-lg h-full">
           <h2
-            className={`${nunitoBold.className} text-xl md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
+            className={`${nunitoBold.className} text-lg md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
           >
             ~3,298fatal and ~2.8 M non-fatal
           </h2>
-          <p>
+          <p className={`${nunitoRegular.className} text-lg text-gray-800`}>
             accidents occur annually across the EU. (Source: Eurostat, 2023)
           </p>
         </div>
       </div>
-      <div className="w-full flex gap-4 justify-center items-center">
-        <div className="w-[50%] bg-violet-200 p-4 m-4 rounded-lg">
+      <div className="w-full flex gap-4 justify-center items-center py-4">
+        <div className="w-[50%] bg-violet-200 p-4 m-4 rounded-lg h-full">
           <h2
-            className={`${nunitoBold.className} text-xl md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
+            className={`${nunitoBold.className} text-lg md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
           >
             STF are the #2 cause
           </h2>
-          <p>
+          <p className={`${nunitoRegular.className} text-lg text-gray-800`}>
             of workplace accidents — leading to severe, long-term injuries.
             (Source: Eurostat, 2023)
           </p>
         </div>
-        <div className="w-[50%] bg-amber-100 p-4 m-4 rounded-lg">
+        <div className="w-[50%] bg-amber-100 p-4 m-4 rounded-lg h-full">
           <h2
             className={`${nunitoBold.className} text-xl md:text-2xl lg:text-4xl xl:text-5xl h-fit overflow-y-hidden tracking-tighter`}
           >
             Lone workers face higher risk
           </h2>
-          <p>
+          <p className={`${nunitoRegular.className} text-xl text-gray-800`}>
             of delayed rescue and undetected incidents.(Source: EU-OSHA, HSE UK)
           </p>
         </div>
