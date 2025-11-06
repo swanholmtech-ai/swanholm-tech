150db5cffd0280fe1d308eb8d59e0cca2dfe8d89
Swanholm Technology <swanholmtech@gmail.com>
2025-11-06 18:28:10 +0100

    save

--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -1,41 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export default function SignupPage() {
+  const t = useTranslations("sign-up");
   return (
     <main className="max-w-7xl mt-40 mx-auto h-[calc(100vh-10rem)]">
       <section className="w-md mx-auto p-2 rounded-sm text-gray-200  md:border md:border-gray-700 md:shadow-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">Signup</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center">{t("signup")}</h1>
 
         <form action="" className="flex flex-col items-center">
           <div className="w-full px-4">
             <div id="email" className="py-2">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email">{t("email")}</label>
               <Input
                 type="email"
                 id="email"
                 name="email"
-                placeholder="Email"
+                placeholder={t("email")}
                 className="w-full"
               />
             </div>
             <div id="password" className="py-2">
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password">{t("password")}</label>
               <Input
                 type="password"
                 id="password"
                 name="password"
-                placeholder="Password"
+                placeholder={t("password")}
                 className="w-full"
               />
             </div>
             <div id="confirm-password" className="py-2">
-              <label htmlFor="confirm-password">Confirm Password</label>
+              <label htmlFor="confirm-password">{t("confirmPassword")}</label>
               <Input
                 type="password"
                 id="confirm-password"
-                placeholder="Confirm Password"
+                placeholder={t("confirmPassword")}
                 name="confirm-password"
               />
             </div>
@@ -45,7 +47,7 @@ export default function SignupPage() {
             variant="outline"
             className="mt-6 mx-auto cursor-pointer hover:bg-gray-600 transition-colors text-gray-700"
           >
-            Sign Up
+            {t("submit")}
           </Button>
         </form>
         <div className="flex justify-between text-sm mt-4">
@@ -54,7 +56,7 @@ export default function SignupPage() {
               href="/login"
               className="text-gray-500 hover:text-gray-300 hover:underline"
             >
-              Already have an account?
+              {t("already-have-account")}
             </Link>
           </p>
           <p className="text-center">
@@ -62,7 +64,7 @@ export default function SignupPage() {
               href="/forgot-password"
               className="text-gray-500 hover:text-gray-300 hover:underline"
             >
-              Forgot Password
+              {t("forgotPassword")}
             </Link>
           </p>
         </div>
