504f60aaf3b6ddc6aa8400324c429d3a03fa2d60
Swanholm Technology <swanholmtech@gmail.com>
2025-11-06 14:47:57 +0100

    save

--- components/view/home-sections/problem-section/SolutionPart.tsx
+++ components/view/home-sections/problem-section/SolutionPart.tsx
@@ -1,8 +1,26 @@
-const SolutionPart = () => {
+import { Nunito_Sans } from "next/font/google";
+
+const nunitoBold = Nunito_Sans({
+  weight: ["700"],
+  subsets: ["latin"],
+});
+
+const nunitoRegular = Nunito_Sans({
+  weight: ["500"],
+  subsets: ["latin"],
+});
+
+const ProblemPart = () => {
   return (
-    <article>
-      <h2>Smart Safety Technology</h2>
-      <p>
+    <article className={`${nunitoRegular.className} w-full h-full mt-12`}>
+      <h2
+        className={`md:text-4xl text-sm ${nunitoBold.className} font-bold tracking-tighter text-gray-800 bg-orange-200 w-fit`}
+      >
+        Solution
+      </h2>
+      <p
+        className={`text-gray-600 text-lg ${nunitoRegular.className} px-20 mt-4`}
+      >
         Swanholm Technology closes the detection gap with Edge AI and a smart
         safety vest. Our ecosystem combines the Alert Vest, Sense Collar, and
         Sense App to detect falls, inactivity, or irregular motion — and trigger
@@ -16,4 +34,4 @@ const SolutionPart = () => {
   );
 };
 
-export default SolutionPart;
+export default ProblemPart;
