6783cfc6ecc8524b504b9238fa3173222b4fd8b1
Swanholm Technology <swanholmtech@gmail.com>
2025-11-11 21:03:23 +0100

    save

--- components/view/vest-hover-effect/VestHoverDetails.tsx
+++ components/view/vest-hover-effect/VestHoverDetails.tsx
@@ -14,13 +14,24 @@ const VestHoverDetails = ({ title }: { title: string }) => {
       >
         {title}
       </h2>
-      <p className="text-lg text-gray-600 text-left">
-        The AI-driven lights illuminate the room, casting a warm and inviting
-        glow that enhances the ambiance. These intelligent lights adjust their
-        brightness and color temperature based on the time of day and the
-        activity in the room, creating a personalized lighting experience that
-        adapts to your needs.
-      </p>
+      {title === "Pockets" ? (
+        <p className="text-lg text-gray-600 text-left mt-4 bg-gray-100 p-4 border border-gray-200 rounded-md">
+          <span className="font-bold">
+            Four different pockets for every taste:
+          </span>
+          <br />
+          Two vertical pockets with flaps at the bottom on the left and right
+          sides, and two zippered pockets on the chest, one on each side.
+        </p>
+      ) : (
+        <p className="text-lg text-gray-600 text-left mt-4">
+          The AI-driven lights illuminate the room, casting a warm and inviting
+          glow that enhances the ambiance. These intelligent lights adjust their
+          brightness and color temperature based on the time of day and the
+          activity in the room, creating a personalized lighting experience that
+          adapts to your needs.
+        </p>
+      )}
     </div>
   );
 };
